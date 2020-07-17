d2cee265721105f5436308a2a2bbad7028c5f1d6
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-17 12:37:36 +0430

    Config React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Scss/App.scss";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./Pages/Hame";
+import Shop from "./Pages/Shop";
+import Blog from "./Pages/Blog";
 
 const App = () => {
   return (
     <>
-      <div className="App">APP</div>
+      <Router>
+        <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/blog" component={Blog} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
     </>
   );
 };
