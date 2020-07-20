fa668bd88f0e2cc1e9933a00b3042a473b79df9f
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-20 11:19:40 +0430

    Fix Style scss in single Component

--- src/Components/Shop/Header.jsx
+++ src/Components/Shop/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <div className="header">
+      <div className="header_shop">
         <Link to="/">خانه</Link>
         <Link to="/shop">فروشگاه</Link>
         <Link to="/blog">بلاگ</Link>
