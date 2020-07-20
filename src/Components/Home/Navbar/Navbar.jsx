14372596548d4643c51cc37dfdd2593df225ce9e
amin-da <darabniya@yahoo.com>
2020-07-20 19:38:03 +0430

    starting design item in navbar

--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div className="header_home_nav">
-        <Link to="/">خانه</Link>
-        <Link to="/shop">فروشگاه</Link>
-        <Link to="/blog">بلاگ</Link>
-        <Link to="/404">خطای ۴۰۴ </Link>
-      </div>
+      <nav className="header_home_nav">
+       <div className='header_home_nav_right'>
+           <Link to="/">خانه</Link>
+            <Link to="/shop">فروشگاه</Link>
+            <Link to="/blog">بلاگ</Link>
+            <Link to="/404">خطای ۴۰۴ </Link>
+       </div>
+      </nav>
     </>
   );
 };
