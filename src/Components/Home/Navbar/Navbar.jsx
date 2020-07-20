c6249870788771dc1c98d3d2809eca601c6cc8fe
amin-da <darabniya@yahoo.com>
2020-07-20 20:08:53 +0430

    adding image and background in navbar

--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -5,14 +5,25 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
+    
       <nav className="header_home_nav">
-       <div className='header_home_nav_right'>
-           <Link to="/">خانه</Link>
-            <Link to="/shop">فروشگاه</Link>
-            <Link to="/blog">بلاگ</Link>
-            <Link to="/404">خطای ۴۰۴ </Link>
-       </div>
+
+        <div className='header_home_nav_right'>
+            <Link to="/">خانه</Link>
+              <Link to="/shop">فروشگاه</Link>
+              <Link to="/blog">بلاگ</Link>
+              <Link to="/404">خطای ۴۰۴ </Link>
+        </div>
+
+        <div className='header_home_nav_left'>
+
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          
+        </div>
       </nav>
+
     </>
   );
 };
