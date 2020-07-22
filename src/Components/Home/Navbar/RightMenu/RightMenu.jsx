e1b2fca75344b89d36cf64604e4a89f333670212
amin-da <darabniya@yahoo.com>
2020-07-22 19:55:45 +0430

    adding some image for test logo design in headear home

--- src/Components/Home/Navbar/RightMenu/RightMenu.jsx
+++ src/Components/Home/Navbar/RightMenu/RightMenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './RightMenu.scss';
+// import logo from '../../../../Images/mega_menu/home/Cizelogo2.jpg'
 import { Link } from "react-router-dom";
 
 
@@ -8,7 +9,7 @@ const RightMenu =()=>{
     return(
 
         <>
-        
+         {/* <img  className='header_home_nav_right_logo' src={logo}/> */}
         <Link to="/"  className='header_home_nav_right_home'>خانه</Link>
         <Link to="/shop"  className='header_home_nav_right_shop'>فروشگاه</Link>
         <Link to="/blog"  className='header_home_nav_right_blog'>بلاگ</Link>
