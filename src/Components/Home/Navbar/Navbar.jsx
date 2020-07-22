6ec23e52d0dc656b648d9ba518cfad1e0b8e501c
amin-da <darabniya@yahoo.com>
2020-07-22 11:35:36 +0430

    adding style in serach bar in headear_home

--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Navbar.scss";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaUserAlt } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import { BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const [openSearch,setOpenSearch]=useState(false)
+
+ const HandleOpen=()=>{
+
+   setOpenSearch(!openSearch)
+
+   const search=document.querySelector('.header_home_nav_left_leftItem_hidden')
+   if (openSearch) {
+    search.classList.remove("search_menu");
+  } else {
+    search.classList.add("search_menu");
+  }
+
+
+ }
+
   return (
     <>
     
@@ -28,7 +46,18 @@ const Navbar = () => {
             </div>
    
             <div className='header_home_nav_left_leftItem'>
-              <a href='#' className='header_home_nav_left_icons_search'> <BsSearch /></a>
+
+                <a href='#' onClick={HandleOpen} >
+                  <BsSearch />
+                </a>
+
+                <div className='header_home_nav_left_leftItem_hidden'>
+                  
+                  <FaTimes  onClick={HandleOpen} className='exit_search'/>
+                  <BsSearch /> 
+                   
+                </div>
+
             </div>
         
          
