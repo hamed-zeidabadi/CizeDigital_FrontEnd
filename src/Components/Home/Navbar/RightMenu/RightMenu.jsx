f866668a9eda74cb21b7b6e449391dd224d40a3e
amin-da <darabniya@yahoo.com>
2020-07-23 10:51:21 +0430

    adding testing element and base style in home mega menu in header home

--- src/Components/Home/Navbar/RightMenu/RightMenu.jsx
+++ src/Components/Home/Navbar/RightMenu/RightMenu.jsx
@@ -10,7 +10,18 @@ const RightMenu =()=>{
 
         <>
          <img  className='header_home_nav_right_logo' src={logo}/>
-        <Link to="/"  className='header_home_nav_right_home'>خانه</Link>
+         
+
+        <Link to="/"  className='header_home_nav_right_home has_sub'>خانه
+
+            <div className='header_home_nav_right_home_menu sub_menu'>
+                <div className="">ss</div>
+                <div className="">ww</div>
+            </div>
+
+        </Link>
+
+
         <Link to="/shop"  className='header_home_nav_right_shop'>فروشگاه</Link>
         <Link to="/blog"  className='header_home_nav_right_blog'>بلاگ</Link>
         <Link to="/404" >خطای ۴۰۴ </Link>
