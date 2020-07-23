5643e7291d22f8dbbfc327ee7d0bd84c72307224
amin-da <darabniya@yahoo.com>
2020-07-23 12:07:39 +0430

    end of working on design and style mega menu home in right navbar in home page

--- src/Components/Home/Navbar/RightMenu/RightMenu.jsx
+++ src/Components/Home/Navbar/RightMenu/RightMenu.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './RightMenu.scss';
 import logo from '../../../../Images/mega_menu/home/output.png'
+import modern from '../../../../Images/mega_menu/home/home-modern.jpg'
+import elegant from '../../../../Images/mega_menu/home/home-elegant.jpg'
+import beauti from '../../../../Images/mega_menu/home/home-metro.jpg'
+import smart from '../../../../Images/mega_menu/home/home-stylist.jpg'
 import { Link } from "react-router-dom";
 
 
@@ -12,14 +16,40 @@ const RightMenu =()=>{
          <img  className='header_home_nav_right_logo' src={logo}/>
          
 
-        <Link to="/"  className='header_home_nav_right_home has_sub'>خانه
+        <Link to="/"  className='header_home_nav_right_home has_sub_menu'>خانه
 
             <div className='header_home_nav_right_home_menu sub_menu'>
-                <div className="">ss</div>
-                <div className="">ww</div>
+
+                <div className="header_home_nav_right_home_menu_image">
+
+                    <div className="header_home_nav_right_home_menu_image_item">
+                         <img src= {modern} alt="modern"/>
+                         <p>Home modern</p>
+                    </div>
+
+                    <div className="header_home_nav_right_home_menu_image_item">
+                        <img src= {smart} alt="modern"/>
+                         <p>Home smart</p>
+                    </div>
+
+                    <div className="header_home_nav_right_home_menu_image_item">
+                        <img src= {beauti} alt="modern"/>
+                         <p>Home beauti</p>
+                    </div>
+
+                    <div className="header_home_nav_right_home_menu_image_item">
+                        <img src= {elegant} alt="modern"/>
+                         <p>Home elegant</p>
+                    </div>
+                    
+                   
+                </div>
+
+                <div className="header_home_nav_right_home_menu_insta"></div>
             </div>
 
         </Link>
+
 
 
         <Link to="/shop"  className='header_home_nav_right_shop'>فروشگاه</Link>
