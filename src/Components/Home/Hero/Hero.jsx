85975bb53d9de917051c549788747ca15a63489f
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-25 21:18:34 +0430

    chenge hero

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -6,25 +6,32 @@ import SwiperTop from "./SwiperTop";
 import Loading from "./Loading";
 
 const Hero = () => {
-  const [isLoading, setIsLoading] = useState({
-    right: false,
-    top: false,
-    bottom: false,
-  });
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(true);
+    }, 2000);
+  }, [isLoading]);
 
   return (
     <>
       <div className="header_home_hero">
-        <div className="header_home_hero_right">
-          {isLoading.right ? <Loading /> : <Slider />}
-        </div>
-        <div className="header_home_hero_left">
-          <div className="header_home_hero_left_top">
-            <h3>نقد و بررسی</h3>
-            <SwiperTop />
-          </div>
-          <div className="header_home_hero_left_bottom">پایین</div>
-        </div>
+        {isLoading ? (
+          <>
+            <div className="header_home_hero_right">
+              <Slider />
+            </div>
+            <div className="header_home_hero_left">
+              <div className="header_home_hero_left_top">
+                <h3>نقد و بررسی</h3>
+                <SwiperTop />
+              </div>
+              <div className="header_home_hero_left_bottom">پایین</div>
+            </div>
+          </>
+        ) : (
+          <Loading />
+        )}
       </div>
     </>
   );
--- src/Components/Home/Hero/Loading.jsx
+++ src/Components/Home/Hero/Loading.jsx
@@ -1,4 +1,15 @@
 import React from "react";
 import { SemipolarLoading } from "react-loadingg";
-const Loading = () => <SemipolarLoading />;
+import "./Hero.scss";
+const Loading = () => {
+  return (
+    <>
+      <div className="loading">
+        <SemipolarLoading className="loading_item" size="large" />
+      </div>
+    </>
+  );
+};
 export default Loading;
+
+// style on Hero.scss
