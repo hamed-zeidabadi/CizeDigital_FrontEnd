aa4623818dc9bcadff6198b72157409a66edefd4
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-25 20:47:42 +0430

    add top swiper

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.scss";
 import { Link } from "react-router-dom";
 import Slider from "./Slider";
+import SwiperTop from "./SwiperTop";
 
 const Hero = () => {
   return (
@@ -11,7 +12,10 @@ const Hero = () => {
           <Slider />
         </div>
         <div className="header_home_hero_left">
-          <div className="header_home_hero_left_top">بالا</div>
+          <div className="header_home_hero_left_top">
+            <h3>نقد و بررسی</h3>
+            <SwiperTop />
+          </div>
           <div className="header_home_hero_left_bottom">پایین</div>
         </div>
       </div>
