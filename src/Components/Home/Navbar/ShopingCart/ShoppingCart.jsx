0aba4a772b96e7da64fe718e2c0503881725dc70
amin-da <darabniya@yahoo.com>
2020-07-26 20:00:04 +0430

    end of step 1 styling in shopingcart component

--- src/Components/Home/Navbar/ShopingCart/ShoppingCart.jsx
+++ src/Components/Home/Navbar/ShopingCart/ShoppingCart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import './ShoppingCart.scss'
 
 
@@ -11,10 +12,18 @@ const ShopingCart =()=>{
         <div>
 
           <a href='#' className='header_home_nav_left_icons_cart'> <FaShoppingCart /> </a> 
+
           <div className='basket_container'>
-            <p>salam</p>
-            <p>salam</p>
-            <p>salam</p>
+
+            <div className='basket_cart_items'>
+
+              <FaTimes  className='basket_cart_items_exit_button' />
+              <h3 className='basket_cart_items_title'>سبد خرید شما</h3>
+        
+            </div>
+
+            <h4>سبد خرید شما خالی است</h4>
+            <a href='#' className='basket_start_shoping'>شروع خرید</a>
           </div>
 
         </div>
