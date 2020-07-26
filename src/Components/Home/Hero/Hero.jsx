d2b39dd4f4b0224537ecd37ae06177ca12276e00
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-26 20:51:30 +0430

    add react-image-gallery

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -4,6 +4,7 @@ import Slider from "./Slider";
 import SwiperTop from "./SwiperTop";
 import Loading from "./Loading";
 import SwiperBottom from "./SwiperBottom";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Hero = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -19,16 +20,22 @@ const Hero = () => {
         {isLoading ? (
           <>
             <div className="header_home_hero_right">
-              <Slider />
+              <ScrollAnimation animateIn="fadeIn">
+                <Slider />
+              </ScrollAnimation>
             </div>
             <div className="header_home_hero_left">
               <div className="header_home_hero_left_top">
                 <h3>نقد و بررسی</h3>
-                <SwiperTop />
+                <ScrollAnimation animateIn="wobble" initiallyVisible={true}>
+                  <SwiperTop />
+                </ScrollAnimation>
               </div>
               <div className="header_home_hero_left_bottom">
-                <h3> اسکرول عمودی</h3>
-                <SwiperBottom />
+                <ScrollAnimation animateIn="wobble" initiallyVisible={true}>
+                  <h3> اسکرول عمودی</h3>
+                  <SwiperBottom />
+                </ScrollAnimation>
               </div>
             </div>
           </>
--- src/Components/Home/Hero/Single.jsx
+++ src/Components/Home/Hero/Single.jsx
@@ -5,18 +5,7 @@ const Single = ({ pic, txt }) => {
   return (
     <>
       <div id="content">
-        <ul id="movieposters">
-          <li>
-            <img className="single_img" src={pic} alt="Iron Man 2" />
-            <div className="movieinfo">
-              <h3>{txt}</h3>
-              <p>این یک متن تستی است برای این کامپوننت </p>
-              <a href="#" title="Iron Man 2">
-                خواندن بیشتر
-              </a>
-            </div>
-          </li>
-        </ul>
+       test
       </div>
     </>
   );
