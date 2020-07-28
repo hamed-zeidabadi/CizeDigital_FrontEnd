1bd6f4fb217e2c09455045dd77d976ef5bc0b655
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-07-28 21:05:26 +0430

    addd typig effect

--- src/Components/Home/Hero/Swiper.jsx
+++ src/Components/Home/Hero/Swiper.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import img from "../../../Images/headphone.png";
+import ScrollAnimation from "react-animate-on-scroll";
+import ReactTypingEffect from "react-typing-effect";
+
 
 const Swiper = () => {
   return (
     <>
-      <div id="content">test</div>
+      <div id="content">
+        <p className="content_title">این یک محصول فوق العاده است !</p>
+        <img src={img} alt="product" className="content_img" />
+        <input type="submit" className="content_btn"></input>
+        <ReactTypingEffect
+          className="content_sidetxt"
+          text="بهترین هدفون های دنیا رو از من و امین بخواهید :)" //text=["Hello.", "World!"]
+        />
+       
+      </div>
     </>
   );
 };
