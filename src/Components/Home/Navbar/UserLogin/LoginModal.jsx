f185b2c2979e0bd4ce1473db26f617ae3723ece1
amin-da <darabniya@yahoo.com>
2020-07-30 10:01:56 +0430

    adding loginmodal file for userlogin component

---
+++ src/Components/Home/Navbar/UserLogin/LoginModal.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ModalWrapper} from 'reoverlay';
+import './LoginModal.scss'
+import 'reoverlay/lib/ModalWrapper.css';
+
+
+const LoginModal =()=>{
+
+
+    return(
+
+        <ModalWrapper animation='zoom'>
+            <div  className=' header_home_nav_left_icons_user_modal'>
+                Hello
+            </div>
+        </ModalWrapper>
+    )
+
+}
+
+
+export default LoginModal
--- src/Components/Home/Navbar/UserLogin/UserLogin.jsx
+++ src/Components/Home/Navbar/UserLogin/UserLogin.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Reoverlay } from 'reoverlay';
 import { FaUserAlt } from "react-icons/fa";
+import LoginModal from './LoginModal'
 
 const UserLogin=()=>{
 
 
+    const Login=()=>{
+
+        Reoverlay.showModal(LoginModal)
+        
+    }
 
     return(
 
         <>
 
-         <a href='#' className='header_home_nav_left_icons_user'>
+         <a href='#'
+            onClick={Login}
+            className='header_home_nav_left_icons_user'>
              <FaUserAlt />
          </a>
 
