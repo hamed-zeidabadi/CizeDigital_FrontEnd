bebf791d7e4ceb1d031828f08eecafae343e67a6
amin-da <darabniya@yahoo.com>
2020-08-02 10:22:20 +0430

    moving const Deals = () => {

--- src/Components/Home/Deals/Deals.jsx
+++ src/Components/Home/Deals/Deals.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./Deals.scss";
-import ScrollAnimation from "react-animate-on-scroll";
+
 
 const Deals = () => {
   return (
     <>
-      <ScrollAnimation animateIn="flipInY" animateOut="flipOutY">
-        <div className="deals">
-          <h1>animateOut</h1>
-        </div>
-      </ScrollAnimation>
+      <div className="deals">test animation</div>
     </>
   );
 };
