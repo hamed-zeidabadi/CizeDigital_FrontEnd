9e935cdd4b5c7989c24a01798726408e0ebf8a2c
amin-da <darabniya@yahoo.com>
2020-08-06 19:40:14 +0330

    working on Featuers Compnent and styling

--- src/Components/Home/Featuers/Features.jsx
+++ src/Components/Home/Featuers/Features.jsx
@@ -10,25 +10,28 @@ const Features= () => {
 
         <div className='tabs'>
 
-            <Tabs >
+            <Tabs>
 
                 <div className='tabs_container'>
 
                     <h2>کالاهای منتخب</h2>
 
                     <div className='tabs_container_tab'>
-                        <Tab><a>Tab 1</a></Tab>
-                        <Tab><a>Tab 2</a></Tab>
-                        <Tab><a>Tab 3</a></Tab>
+                        <Tab><a className="hvr-underline-from-center">محبوب ها</a></Tab>
+                        <Tab><a className="hvr-underline-from-center">پرفروش ها</a></Tab>
+                        <Tab><a className="hvr-underline-from-center">جدید ها</a></Tab>
                     </div>
                     
 
                 </div>
 
 
+                <div className='tabs_panel_container'>
                 <Panel><p>Panel 1</p></Panel>
                 <Panel><p>Panel 2</p></Panel>
                 <Panel><p>Panel 3</p></Panel>
+                </div>
+               
                 
 
             </Tabs>
