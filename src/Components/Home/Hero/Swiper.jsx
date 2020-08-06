6b3d86dae05a0fb492098db552976f37fc6d094e
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-08-06 15:13:08 +0430

    remove extra code

--- src/Components/Home/Hero/Swiper.jsx
+++ src/Components/Home/Hero/Swiper.jsx
@@ -1,31 +1,25 @@
 import React from "react";
-import img1 from "../../../Images/headphone4.png";
-import img2 from "../../../Images/watch.png";
-import ScrollAnimation from "react-animate-on-scroll";
 import ReactTypingEffect from "react-typing-effect";
-import './Swiper.scss'
+import "./Swiper.scss";
 
-const SwiperS = ({img,title,sidetxt}) => {
- 
+const SwiperS = ({ img, title, sidetxt }) => {
   return (
     <>
-     
-        <div id="content">
-          <div className="right">
-            <img src={img} alt="product" className="right_img" />
-          </div>
-          <div className="left">
-            <p className="left_title">{title}</p>
-            <ReactTypingEffect
-              className="left_sidetxt"
-              text={sidetxt} //text=["Hello.", "World!"]
-            />
-            <button type="submit" className="left_btn button -dark center">
-              مشاهده محصولات
-            </button>
-          </div>
+      <div id="content">
+        <div className="right">
+          <img src={img} alt="product" className="right_img" />
         </div>
-      )
+        <div className="left">
+          <p className="left_title">{title}</p>
+          <ReactTypingEffect
+            className="left_sidetxt"
+            text={sidetxt} //text=["Hello.", "World!"]
+          />
+          <button type="submit" className="left_btn button -dark center">
+            مشاهده محصولات
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- src/Components/Home/Hero/SwiperJS.jsx
+++ src/Components/Home/Hero/SwiperJS.jsx
@@ -3,25 +3,19 @@ import "./Swiper.scss";
 
 // import Swiper core and required components
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { EffectFade, EffectCube, EffectCoverflow, EffectFlip } from "swiper";
-import "swiper/components/effect-fade/effect-fade.scss";
-import "swiper/components/effect-cube/effect-cube.scss";
-import "swiper/components/effect-coverflow/effect-coverflow.scss";
+import { EffectFlip } from "swiper";
 import "swiper/components/effect-flip/effect-flip.scss";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
-import "swiper/components/scrollbar/scrollbar.scss";
 import SwiperS from "./Swiper";
 import img1 from "../../../Images/headphone4.png";
 import img2 from "../../../Images/watch.png";
 
 // install Swiper components
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
-SwiperCore.use([EffectFade, EffectCube, EffectCoverflow, EffectFlip]);
+SwiperCore.use([EffectFlip]);
 
 export default () => {
   const [state, setstate] = React.useState([
@@ -44,11 +38,8 @@ export default () => {
       Autoplay
       spaceBetween={100}
       slidesPerView={3}
-      // navigation
       pagination={{ clickable: true }}
-      // scrollbar={{ draggable: true }}
-      // onSwiper={(swiper) => console.log(swiper)}
-      // onSlideChange={() => console.log("slide change")}
+      
     >
       {state.map((item) => (
         <SwiperSlide className="swiper_main_item">
