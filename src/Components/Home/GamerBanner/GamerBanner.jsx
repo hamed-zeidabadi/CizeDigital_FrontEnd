f498161aa799612474891d635051615ee82b9451
amin-da <darabniya@yahoo.com>
2020-08-11 18:54:13 +0330

    chaging header text and image in GamerBanner component !

--- src/Components/Home/GamerBanner/GamerBanner.jsx
+++ src/Components/Home/GamerBanner/GamerBanner.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import "./GamerBanner.scss"
 
-import dual from "../../../Images/gamer_banner/dual.png"
+// import dual from "../../../Images/gamer_banner/dual.png"
+import dual from "../../../Images/headphone3.png"
 
 const GamerBanner = () => {
 
@@ -11,7 +12,7 @@ const GamerBanner = () => {
 
             <div className='gamer_banner_text_container'>
 
-              <p className="gamer_banner_text_title">گیمرهای حرفه ای</p>
+              <p className="gamer_banner_text_title">عاشقان موسیقی</p>
               <p className=" gamer_banner_text_discount">تا 30 درصد برای بروزرسانی تجهیزات تخفیف بگیرید</p>
               <a className='btn effect'><span>فروشگاه</span></a>
 
