771402919e6593613e6069b32b4b6efc61218c99
amin-da <darabniya@yahoo.com>
2020-08-11 12:11:49 +0330

    adding images and element on GamerBanner component and finishin styling thems

--- src/Components/Home/GamerBanner/GamerBanner.jsx
+++ src/Components/Home/GamerBanner/GamerBanner.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import "./GamerBanner.scss"
 
+import dual from "../../../Images/gamer_banner/dual.png"
+
 const GamerBanner = () => {
 
     return (
+
         <div className='gamer_banner_container'>
 
             <div className='gamer_banner_text_container'>
-            <p className="gamer_banner_text_title">گیمرهای حرفه ای</p>
-            <p className="gamer_banner_text_discount">تا 30 درصد برای بروزرسانی تجهیزات تخفیف بگیرید</p>
+
+              <p className="gamer_banner_text_title">گیمرهای حرفه ای</p>
+              <p className=" gamer_banner_text_discount">تا 30 درصد برای بروزرسانی تجهیزات تخفیف بگیرید</p>
+              <a className='btn effect'><span>فروشگاه</span></a>
+
             </div>
            
-            <img />
+            <img className='gamer_banner_image' src={dual}/>
+
         </div>
     )
 }
