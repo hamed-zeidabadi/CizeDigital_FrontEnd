4ec3b7ff3be3b35282d4aee456176dbac65ca0df
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-08-16 19:13:33 +0430

    run responsive hover in hero => blog

--- src/Components/Blog/Hero/Hero.jsx
+++ src/Components/Blog/Hero/Hero.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from "react";
 import "./Hero.scss";
-import ReactTypingEffect from "react-typing-effect";
 import "./hover.scss";
 
+import ReactTypingEffect from "react-typing-effect";
+
 const Hero = () => {
-  const [isActive, setisActive] = useState(false);
+  const [isActive, setisActive] = useState({
+    link1: false,
+    link2: false,
+    link3: false,
+    link4: false,
+  });
   const text = [
     "داغ ترین اخبار روز دنیای تکنولوژی",
     "معرفی آخرین فناوری های روز دنیا",
@@ -32,28 +38,54 @@ const Hero = () => {
             <li>
               <a
                 className={
-                  isActive
+                  isActive.link1
                     ? "hvr_line_active hvr-wobble-vertical"
                     : "hvr_line_disable hvr-wobble-vertical"
                 }
                 href="#"
-                onClick={setisActive(!isActive)}
+                onClick={() => setisActive({ link1: !isActive.link1 })}
               >
                 همه
               </a>
             </li>
             <li>
-              <a className="  hvr-wobble-vertical" href="#">
+              <a
+                className={
+                  isActive.link2
+                    ? "hvr_line_active hvr-wobble-vertical"
+                    : "hvr_line_disable hvr-wobble-vertical"
+                }
+                href="#"
+                onClick={() => setisActive({ link2: !isActive.link2 })}
+              >
                 تکنولوژی
               </a>
             </li>
             <li>
-              <a className=" hvr_line hvr-wobble-vertical" href="#">
+              <a
+                className={
+                  isActive.link3
+                    ? "hvr_line_active hvr-wobble-vertical"
+                    : "hvr_line_disable hvr-wobble-vertical"
+                }
+                href="#"
+                onClick={() => setisActive({ link3: !isActive.link3 })}
+              
+              >
                 سبک زندگی
               </a>
             </li>
             <li>
-              <a className=" hvr_line hvr-wobble-vertical" href="#">
+              <a
+                className={
+                  isActive.link4
+                    ? "hvr_line_active hvr-wobble-vertical"
+                    : "hvr_line_disable hvr-wobble-vertical"
+                }
+                href="#"
+                onClick={() => setisActive({ link4: !isActive.link4 })}
+              
+              >
                 طراحی
               </a>
             </li>
