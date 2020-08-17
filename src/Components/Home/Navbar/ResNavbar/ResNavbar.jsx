0a51bbf38d01a392e230b07faf62de03f9ffea17
amin-da <darabniya@yahoo.com>
2020-08-17 10:06:54 +0330

    adding items in a ResNavbar Component and deleting unnessary image and chanign background in a navbar home ! :)

--- src/Components/Home/Navbar/ResNavbar/ResNavbar.jsx
+++ src/Components/Home/Navbar/ResNavbar/ResNavbar.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import './ResNavbar.scss'
+import { BsSearch } from "react-icons/bs";
+import { FaBars } from "react-icons/fa";
+import logoSite from "../../../../Images/mega_menu/home/output.png"
 
 const ResNavbar = () => {
     return (
-       
+      
         <>
     
         <nav className="header_home_res_nav">
+
+          <div className='header_home_res_nav_right'> 
+
+              <div className='header_home_res_nav_right_menu'>
+
+                <a href='#' >
+                      <FaBars />
+                  </a>
+
+                  <a href='#' >
+                      <BsSearch />
+                  </a>
+
+              </div>
+
+             <div className='header_home_res_nav_right_siteIcon'>
+
+              <img  src={logoSite}/>
+
+             </div>
+
+            
+          </div>
+
+          
+          <div className='header_home_res_nav_left'>
+
+
+          </div>
   
         </nav>
   
