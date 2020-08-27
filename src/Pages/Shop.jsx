cae5569db4630326808771b773538bc1e6de274f
amin-da <darabniya@yahoo.com>
2020-08-27 12:22:45 +0330

    start working on Shop page and adding sample of card for each product

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./../Components/Shop/Header";
+import ShopgCart from "./../Components/Shop/Cart/ShopCart"
 const Shop = () => {
   return (
     <>
       <div className="shop">
         <Header />
         فروشگاه
+        <ShopgCart />
       </div>
     </>
   );
