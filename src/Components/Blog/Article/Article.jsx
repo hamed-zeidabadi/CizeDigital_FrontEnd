3319b227132a95c8c2e85330380ec6436a68e2ad
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-08-28 13:07:43 +0430

    import image for blog

--- src/Components/Blog/Article/Article.jsx
+++ src/Components/Blog/Article/Article.jsx
@@ -3,10 +3,60 @@ import "./Article.scss";
 import Card from "./Card";
 
 const Article = () => {
+  const data = [
+    {
+      id: 1,
+      title: "نسخه ۲۰۰۴ ویندوز ۱۰ به ssd آسیب می‌زند",
+      desc:
+        "برخی کاربران باگی را در ابزار Optimize Drives ویندوز ۱۰ شناسایی کرده‌اند که می‌تواند بر طول عمر حافظه‌های SSD تأثیر منفی بگذارد.",
+      category: "تکنولوژی",
+      img: require("../../../Images/Blog/1.jpg"),
+      date: "25 آبان 1399",
+    },
+    {
+      id: 2,
+      title: "اینتل چطور در سال‌های پیش رو با قانون مور همگام‌ باقی می‌ماند؟",
+      desc:
+        "ترانزیستورها در یک چیپ کامپیوتری خواهیم بود",
+      category: "تکنولوژی",
+      img: require("../../../Images/Blog/2.jpg"),
+      date: "26 آبان 1399",
+    },
+    {
+      id: 3,
+      title: "شبکه بیت‌کوین حالا به اندازه ۷ نیروگاه هسته‌ای برق مصرف می‌کند",
+      desc:
+        "کوین حالا به اندازه هفت نیروگاه هسته‌ای برق مصرف می‌کند.",
+      category: "اخبار",
+      img: require("../../../Images/Blog/3.jpg"),
+      date: "27 آبان 1399",
+    },
+    {
+      id: 4,
+      title: "بازی‌های موبایل ایرانی که گرافیک بالایی دارند",
+      desc:
+        "گرافیک بالا و بازی‌های ایرانی؛ دو کلمه‌ای که شاید تا چند سال گذشته آن‌قدرها کنار هم جور در نمی‌آمدند ولی با پیشرفت",
+      category: "سبک زندگی",
+      img: require("../../../Images/Blog/4.jpg"),
+      date: "28 آبان 1399",
+    },
+  ];
+
   return (
     <>
       <div className="article_blog">
-        <Card />
+        {data
+          ? data.map((items) => (
+              <Card
+                key={items.id}
+                title={items.title}
+                desc={items.desc}
+                category={items.category}
+                img={items.img}
+                date={items.date}
+              />
+            ))
+          : null}
       </div>
     </>
   );
--- src/Components/Blog/Article/Card.jsx
+++ src/Components/Blog/Article/Card.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Card.scss";
 import { FaChevronCircleLeft } from "react-icons/fa";
-import pic from "../../../Images/Blog/1.jpg";
+// import pic from "../../../Images/Blog/1.jpg";
 
 const CardHeader = (props) => {
-  const { image } = props;
+  const { image, category } = props;
   var style = {
     backgroundImage: "url(" + image + ")",
   };
   return (
     <header style={style} id={image} className="card-header">
-      <h4 className="card-header--title">اخبار</h4>
+      <h4 className="card-header--title">{category}</h4>
     </header>
   );
 };
@@ -18,36 +18,31 @@ const CardHeader = (props) => {
 const Button = (props) => {
   return (
     <button className="button button-primary">
-     <FaChevronCircleLeft />
+      <FaChevronCircleLeft />
       متن کامل مقاله
     </button>
   );
 };
 
-const CardBody = (props) => {
+const CardBody = ({ date, title, desc }) => {
   return (
     <div className="card-body">
-      <p className="date">25 آبان 1399</p>
+      <p className="date">{date}</p>
 
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
 
-      <p className="body-content">{props.text}</p>
+      <p className="body-content">{desc}</p>
 
       <Button />
     </div>
   );
 };
 
-const Card = (props) => {
+const Card = ({ title, desc, category, img, date }) => {
   return (
     <article className="card">
-      <CardHeader image={pic} />
-      <CardBody
-        title={"بهترین هدفون دنیا چیه ؟"}
-        text={
-          "این یک متن تستی است ، این یک متن تستی است ، این یک متن تستی است "
-        }
-      />
+      <CardHeader image={img} category={category} />
+      <CardBody title={title} desc={desc} category={category} date={date} />
     </article>
   );
 };
