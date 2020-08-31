0d12284c2a5a9c21c3755ce33c8dd1c246f7b2d7
amin-da <darabniya@yahoo.com>
2020-08-31 18:44:04 +0330

    adding section on shop page

--- src/Components/Shop/Products/Products.jsx
+++ src/Components/Shop/Products/Products.jsx
@@ -6,8 +6,20 @@ const Products = () => {
 
         <div className='product_container'>
             
-            <div className="product_items"></div>
-            <div className='product_divisions'></div>
+            <div className="product_items">
+
+                <div className="product_items_filter">فیلتر</div>
+                <div className="product_items_cards"></div>
+
+            </div>
+
+            <div className='product_divisions'>
+
+                <div className='product_divisions_faction'>دسته بندی ها</div>
+                <div className='product_divisions_price'>قیمت</div>
+                <div className='product_divisions_brands'>برندها</div>
+
+            </div>
         </div>
 
     )
