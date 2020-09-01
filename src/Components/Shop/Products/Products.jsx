de57f1e16baedb2029be760aa9de4dad6a5d9e96
amin-da <darabniya@yahoo.com>
2020-09-01 12:39:20 +0330

    adding some elemnst on product_divisions in Products component in Shop page !

--- src/Components/Shop/Products/Products.jsx
+++ src/Components/Shop/Products/Products.jsx
@@ -9,24 +9,39 @@ const Products = () => {
 
             <div className='product_divisions'>
 
-            <div className='product_divisions_faction'>
-            
-            <ul>
-                <li> <a href=''>هدفون ها</a> </li>
-                <li> <a href=''>اسپیکر</a> </li>
-                <li> <a href=''>کیس</a> </li>
-                <li> <a href=''>محافظ صفحه</a> </li>
-                <li> <a href=''>گیمینگ</a> </li>
-                <li> <a href=''>پاور</a> </li>
-                <li> <a href=''>کابل ها</a> </li>
-                <li> <a href=''>لوازم جانبی</a> </li>
-            </ul>
+                <div className='product_divisions_faction'>
 
-            </div>
+                        <h3>دسته ها</h3>
+                    <ul>
+                        <li> <a href=''>هدفون ها</a> </li>
+                        <li> <a href=''>اسپیکر</a> </li>
+                        <li> <a href=''>کیس</a> </li>
+                        <li> <a href=''>محافظ صفحه</a> </li>
+                        <li> <a href=''>گیمینگ</a> </li>
+                        <li> <a href=''>پاور</a> </li>
+                        <li> <a href=''>کابل ها</a> </li>
+                        <li> <a href=''>لوازم جانبی</a> </li>
+                    </ul>
 
+                </div>
 
-            <div className='product_divisions_price'>قیمت</div>
-            <div className='product_divisions_brands'>برندها</div>
+                <div className='product_divisions_price'>
+                  <h3>براساس قیمت</h3>
+                </div>
+
+                <div className='product_divisions_brands'>
+
+                    <h3>برندها</h3>
+                    <ul>
+                        <li> <a href=''>Boss</a> </li>
+                        <li> <a href=''>Senhiser</a> </li>
+                        <li> <a href=''>Beats</a> </li>
+                        <li> <a href=''>Audio Technica</a> </li>
+                        <li> <a href=''>Klipesch</a> </li>
+                       
+                    </ul>
+
+                </div>
 
             </div>
             
