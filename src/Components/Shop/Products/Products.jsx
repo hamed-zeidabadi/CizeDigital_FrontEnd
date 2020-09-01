0540f1411f11ce7a549eeaa2eb85d8f70f434421
amin-da <darabniya@yahoo.com>
2020-09-01 12:21:14 +0330

    adding elements in product_divisions_faction in Products component in shop page

--- src/Components/Shop/Products/Products.jsx
+++ src/Components/Shop/Products/Products.jsx
@@ -6,6 +6,29 @@ const Products = () => {
     return (
 
         <div className='product_container'>
+
+            <div className='product_divisions'>
+
+            <div className='product_divisions_faction'>
+            
+            <ul>
+                <li> <a href=''>هدفون ها</a> </li>
+                <li> <a href=''>اسپیکر</a> </li>
+                <li> <a href=''>کیس</a> </li>
+                <li> <a href=''>محافظ صفحه</a> </li>
+                <li> <a href=''>گیمینگ</a> </li>
+                <li> <a href=''>پاور</a> </li>
+                <li> <a href=''>کابل ها</a> </li>
+                <li> <a href=''>لوازم جانبی</a> </li>
+            </ul>
+
+            </div>
+
+
+            <div className='product_divisions_price'>قیمت</div>
+            <div className='product_divisions_brands'>برندها</div>
+
+            </div>
             
             <div className="product_items">
 
@@ -16,13 +39,7 @@ const Products = () => {
 
             </div>
 
-            <div className='product_divisions'>
-
-                <div className='product_divisions_faction'>دسته بندی ها</div>
-                <div className='product_divisions_price'>قیمت</div>
-                <div className='product_divisions_brands'>برندها</div>
-
-            </div>
+     
         </div>
 
     )
