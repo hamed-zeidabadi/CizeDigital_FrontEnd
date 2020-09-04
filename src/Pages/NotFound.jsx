b1c3fe41f393f02b2894e0df193a6724e3710562
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-09-04 08:42:27 +0430

    add svg in => 404 component

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../Images/404/logo.svg";
+import astronaut from "../Images/404/moon.svg";
+import moon from "../Images/404/astronaut.svg";
+import earth from "../Images/404/earth.svg";
+import rocket from "../Images/404/rocket.svg";
+import err404 from "../Images/404/404.svg";
+
+
 const NotFound = () => {
   return (
     <>
@@ -9,44 +16,29 @@ const NotFound = () => {
           <div class="central-body">
             <img
               class="image-404"
-              src="http://salehriaz.com/404Page/img/404.svg"
+              src={err404}
               width="300px"
             />
-            <a
-              href="http://salehriaz.com/404Page/404.html"
+            <Link
+              to="/"
               class="btn-go-home"
-              target="_blank"
+              // target="_blank"
             >
-              GO BACK HOME
-            </a>
+              برگرد به خانه !
+            </Link>
           </div>
           <div class="objects">
-            <img
-              class="object_rocket"
-              src="http://salehriaz.com/404Page/img/rocket.svg"
-              width="40px"
-            />
+            <img class="object_rocket" src={rocket} width="40px" />
             <div class="earth-moon">
-              <img
-                class="object_earth"
-                src="http://salehriaz.com/404Page/img/earth.svg"
-                width="100px"
-              />
-              <img
-                class="object_moon"
-                src="http://salehriaz.com/404Page/img/moon.svg"
-                width="80px"
-              />
+              <img class="object_earth" src={earth} width="100px" />
+              <img class="object_moon" src={moon} width="80px" />
             </div>
             <div class="box_astronaut">
-              <img
-                class="object_astronaut"
-                src="http://salehriaz.com/404Page/img/astronaut.svg"
-                width="140px"
-              />
+              <img class="object_astronaut" src={astronaut} width="140px" />
             </div>
           </div>
           <div class="glowing_stars">
+            <div class="star"></div>
             <div class="star"></div>
             <div class="star"></div>
             <div class="star"></div>
